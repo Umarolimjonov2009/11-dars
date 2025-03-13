a09532add7e4a2075ab55ff58f37d70f0b7713ae
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-03-13 17:56:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+
+const body=document.getElementsByTagName("body")[0];
+const p=document.getElementById("p");
+
+const rang1=prompt("body rangini kiriting");
+const rang2=prompt("text rangini kiriting");
+body.style.backgroundColor=rang1;
+p.style.color=rang2;
